17db1870246bc0812d19cec0194a07107a6bcbe3
Artur Karbone <artur.karbone@gmail.com>
2017-06-09 12:44:51 +0300

    Media feature: get/delete

--- src/Instagram.Private.API/Client/MediaClient.cs
+++ src/Instagram.Private.API/Client/MediaClient.cs
@@ -168,5 +168,165 @@ namespace Instagram.Private.API.Client.Direct
 
             return response;
         }
+
+        public async Task<MediaResponse> Get(string id)
+        {
+
+            var response = wrapper
+                .SetResource($"media/{id}/info/")
+                .GetAsync().Result
+                .Content.ReadAsStringAsync().Result
+                .Deserialize<MediaResponse>();
+
+            return response;
+        }
+
+        public async Task<MediaResponse> Get(Uri uri)
+        {
+            var oembed = await Oembed(uri);
+            return await Get(oembed.media_id);
+        }
+
+
+
+        public async Task<MediaResponse> Delete(string id)
+        {
+            var payload = new
+            {
+                _uuid = Guid.NewGuid().ToString(),
+                device_id = this.device.Id,
+                _csrftoken = wrapper.GetCookieValue("csrftoken"),
+                media_id = id,
+            };
+
+            var response = wrapper
+                .SetResource($"media/{id}/delete/?media_type=PHOTO ")
+                .PostSigned(payload).Result
+                .Content.ReadAsStringAsync().Result
+                .Deserialize<MediaResponse>();
+
+            return response;
+        }
+
+        public async Task<MediaResponse> Delete(Uri uri)
+        {
+            var oembed = await Oembed(uri);
+            return await Delete(oembed.media_id);
+        }
+
+        protected async Task<OembedResponse> Oembed(Uri uri)
+        {
+            var res = new HttpClientWrapper()
+                    .SetBaseAddress($"https://api.instagram.com/oembed?url={uri}")
+                    .GetAsync().Result.Content.ReadAsStringAsync().Result
+                    .Deserialize<OembedResponse>();
+            return res;
+        }
     }
+
+
+    public class MediaResponse
+    {
+        public Item[] items { get; set; }
+        public int num_results { get; set; }
+        public bool more_available { get; set; }
+        public bool auto_load_more_enabled { get; set; }
+        public string status { get; set; }
+
+
+        public class Item
+        {
+            public int taken_at { get; set; }
+            public long pk { get; set; }
+            public string id { get; set; }
+            public long device_timestamp { get; set; }
+            public int media_type { get; set; }
+            public string code { get; set; }
+            public string client_cache_key { get; set; }
+            public int filter_type { get; set; }
+            public Image_Versions2 image_versions2 { get; set; }
+            public int original_width { get; set; }
+            public int original_height { get; set; }
+            public User user { get; set; }
+            public string organic_tracking_token { get; set; }
+            public int like_count { get; set; }
+            public string[] top_likers { get; set; }
+            public bool has_liked { get; set; }
+            public bool comment_likes_enabled { get; set; }
+            public bool has_more_comments { get; set; }
+            public int max_num_visible_preview_comments { get; set; }
+            public object[] preview_comments { get; set; }
+            public int comment_count { get; set; }
+            public Caption caption { get; set; }
+            public bool caption_is_edited { get; set; }
+            public bool photo_of_you { get; set; }
+            public bool can_viewer_save { get; set; }
+        }
+
+        public class Image_Versions2
+        {
+            public Candidate[] candidates { get; set; }
+        }
+
+        public class Candidate
+        {
+            public int width { get; set; }
+            public int height { get; set; }
+            public string url { get; set; }
+        }
+
+        public class User
+        {
+            public long pk { get; set; }
+            public string username { get; set; }
+            public string full_name { get; set; }
+            public bool is_private { get; set; }
+            public string profile_pic_url { get; set; }
+            public string profile_pic_id { get; set; }
+            public bool is_verified { get; set; }
+            public bool has_anonymous_profile_picture { get; set; }
+            public bool can_boost_post { get; set; }
+            public bool can_see_organic_insights { get; set; }
+            public bool show_insights_terms { get; set; }
+            public bool is_unpublished { get; set; }
+        }
+
+        public class Caption
+        {
+            public long pk { get; set; }
+            public long user_id { get; set; }
+            public string text { get; set; }
+            public int type { get; set; }
+            public int created_at { get; set; }
+            public int created_at_utc { get; set; }
+            public string content_type { get; set; }
+            public string status { get; set; }
+            public int bit_flags { get; set; }
+            public User user { get; set; }
+            public long media_id { get; set; }
+        }
+    }
+
+
+
+
+    public class OembedResponse
+    {
+        public string version { get; set; }
+        public string title { get; set; }
+        public string author_name { get; set; }
+        public string author_url { get; set; }
+        public long author_id { get; set; }
+        public string media_id { get; set; }
+        public string provider_name { get; set; }
+        public string provider_url { get; set; }
+        public string type { get; set; }
+        public int width { get; set; }
+        public object height { get; set; }
+        public string html { get; set; }
+        public string thumbnail_url { get; set; }
+        public int thumbnail_width { get; set; }
+        public int thumbnail_height { get; set; }
+    }
+
 }
--- tests/Instagram.Private.API.Tests/MediaTests.cs
+++ tests/Instagram.Private.API.Tests/MediaTests.cs
@@ -1,29 +1,62 @@
 ﻿using Shouldly;
+using System;
 using System.Linq;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace Instagram.Private.API.Tests
 {
     public class MediaTests
     {
-        [Fact(Skip = "used ad hoc")]
+        //[Fact(Skip = "used ad hoc")]
+        //[Fact]
+        //[Trait("Media", "Functional")]
+        /////dotnet test --filter "FullyQualifiedName=Instagram.Private.API.Tests.MediaTests.Should_Configure_Photo"
+
+        //public void Should_Configure_Photo()
+        //{
+        //    var photo =
+        //    Fixtures.Clients.Default
+        //      .Media
+        //      .UploadPhoto(@"C:\temp\img1.jpg").Result;
+
+        //    photo.status.ShouldBe("ok");
+
+        //    Fixtures.Clients.Default
+        //       .Media
+        //       .ConfigurePhoto(photo.upload_id, "Chatting with my friend #friends").Result
+        //       .status
+        //       .ShouldBe("ok");
+        //}
+
+
+        [Fact(Skip = "used ad hoc")]       
         [Trait("Media", "Functional")]
         ///dotnet test --filter "FullyQualifiedName=Instagram.Private.API.Tests.MediaTests.Should_Configure_Photo"
 
-        public void Should_Configure_Photo()
+        public async Task Should_Obtain_Media_By_Url()
         {
-            var photo =
+            var media = await
             Fixtures.Clients.Default
               .Media
-              .UploadPhoto(@"C:\temp\img1.jpg").Result;
+              .Get(new Uri("https://www.instagram.com/p/BVHSfdWBmI9/?taken-by=jennygreene_91"));
 
-            photo.status.ShouldBe("ok");
+            media.status.ShouldBe("ok");
+        }
 
+        [Fact]
+        [Trait("Media", "Functional")]
+        ///dotnet test --filter "FullyQualifiedName=Instagram.Private.API.Tests.MediaTests.Should_Configure_Photo"
+
+        public async Task Should_Delete_Media_By_Url()
+        {
+            var media = await
             Fixtures.Clients.Default
-               .Media
-               .ConfigurePhoto(photo.upload_id, "Chatting with my friend #friends").Result
-               .status
-               .ShouldBe("ok");
-        }       
+              .Media
+              .Delete(new Uri("https://www.instagram.com/p/BVHTZ6zhybP/?taken-by=jennygreene_91"));
+
+            media.status.ShouldBe("ok");
+        }      
+
     }
 }
